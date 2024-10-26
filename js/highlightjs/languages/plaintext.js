9394f5b579bb64400babb962f1dde5d455861fa1
varun7654 <varun7654@outlook.com>
2024-10-26 17:27:28 -0400

    refactor files

--- js/highlightjs/languages/plaintext.js
+++ js/highlightjs/languages/plaintext.js
@@ -1,29 +1,29 @@
 /*! `plaintext` grammar compiled for Highlight.js 11.10.0 */
-  (function(){
+(function () {
     var hljsGrammar = (function () {
-  'use strict';
+        'use strict';
 
-  /*
-  Language: Plain text
-  Author: Egor Rogov (e.rogov@postgrespro.ru)
-  Description: Plain text without any highlighting.
-  Category: common
-  */
+        /*
+        Language: Plain text
+        Author: Egor Rogov (e.rogov@postgrespro.ru)
+        Description: Plain text without any highlighting.
+        Category: common
+        */
 
-  function plaintext(hljs) {
-    return {
-      name: 'Plain text',
-      aliases: [
-        'text',
-        'txt'
-      ],
-      disableAutodetect: true
-    };
-  }
+        function plaintext(hljs) {
+            return {
+                name: 'Plain text',
+                aliases: [
+                    'text',
+                    'txt'
+                ],
+                disableAutodetect: true
+            };
+        }
 
-  return plaintext;
+        return plaintext;
 
-})();
+    })();
 
     hljs.registerLanguage('plaintext', hljsGrammar);
-  })();
+})();
